a2c30e376f33aab4d0b2639e6b499595bf430efa
mamoon-07 <shahmamoonakbar@gmail.com>
2026-03-09 15:20:38 +0500

    Assignment Operator

--- hello.js
+++ hello.js
@@ -4,3 +4,16 @@ console.log("hello Everyone");
 
 /* This is a multi-line comment
    that spans multiple lines */
+
+// Data Types and Variables in JavaScript
+
+var name = "John"; // String
+let age = 30; // Number
+const isStudent = true; // Boolean  
+
+// Assignment Operators
+var a = 5; // age is now 35
+var b = 35; 
+var c = 33;
+var d = 66;
+var sum = a + b + c + d; // sum is now 139
