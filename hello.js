24d893222c095044d4104a75734488f0931a706b
mamoon-07 <shahmamoonakbar@gmail.com>
2026-03-10 11:11:12 +0500

    Counting Cards Example

--- hello.js
+++ hello.js
@@ -292,3 +292,34 @@ function myFunction(a, b) {
 console.log(myFunction(5, 3)); // Output: 8
 console.log(myFunction(-1, 3)); // Output: Negative numbers are not allowed
 console.log(myFunction(5, -2)); // Output: Negative numbers are not allowed
+
+// Counting Cards Example
+var count = 0;
+function cc(card) {
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
+    }       
+    if (count > 0) {
+        return count + " Bet"; // Returns the current count and "Bet" if the count is positive
+    } else {
+        return count + " Hold"; // Returns the current count and "Hold" if the count is zero or negative
+    }
+}
+console.log(cc(2)); // Output: 1 Bet
+console.log(cc(3)); // Output: 2 Bet
+console.log(cc(7)); // Output: 2 Bet
+console.log(cc("K")); // Output: 1 Bet
+console.log(cc("A")); // Output: 0 Hold
