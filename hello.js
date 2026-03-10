078d34dd2cb30488aa8f57ffee6cad14909408f2
mamoon-07 <shahmamoonakbar@gmail.com>
2026-03-10 11:01:57 +0500

    Switch Statement

--- hello.js
+++ hello.js
@@ -250,3 +250,27 @@ console.log(golfScore(5, 6)); // Output: Bogey
 console.log(golfScore(5, 7)); // Output: Double Bogey
 console.log(golfScore(5, 8)); // Output: Go Home!   
 
+
+// Switch Statements
+function caseInSwitch(val) {
+    var answer = "";
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    return answer;
+}
+console.log(caseInSwitch(1)); // Output: alpha
+console.log(caseInSwitch(2)); // Output: beta
+console.log(caseInSwitch(3)); // Output: gamma
+console.log(caseInSwitch(4)); // Output: delta
